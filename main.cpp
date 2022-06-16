2736434183fa20a780dab3b1242a70831d8bc1dd
kevinchang <kevinjchang98@gmail.com>
2022-06-16 01:37:15 -0700

    remove demo windows, implement basic text chat window

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 
+#include <iostream>  // TODO: Remove later when not needed
+
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
@@ -108,8 +110,6 @@ int main(int, char**) {
     // NULL, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != NULL);
 
     // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop
@@ -131,58 +131,28 @@ int main(int, char**) {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in
-        // ImGui::ShowDemoWindow()! You can browse its code to learn more about
-        // Dear ImGui!).
-        if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End
-        // pair to created a named window.
+        /**
+         * @brief Show basic chat window
+         */
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            // Create window
+            ImGui::Begin("Text input window");
 
-            ImGui::Begin("Hello, world!");  // Create a window called "Hello,
-                                            // world!" and append into it.
+            // Initial text
+            static char text[1024 * 16] = "";
 
-            ImGui::Text(
-                "This is some useful text.");  // Display some text (you can use
-                                               // a format strings too)
-            ImGui::Checkbox("Demo Window",
-                            &show_demo_window);  // Edit bools storing our
-                                                 // window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
+            // Add text input area
+            ImGui::InputTextMultiline(
+                "##source", text, IM_ARRAYSIZE(text),
+                ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 16));
 
-            ImGui::SliderFloat(
-                "float", &f, 0.0f,
-                1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3(
-                "clear color",
-                (float*)&clear_color);  // Edit 3 floats representing a color
+            // Send button
+            // TODO: Currently just cout's contents
+            if (ImGui::Button("Send")) {
+                std::cout << "Send button pressed with text contents: " << text
+                          << std::endl;
+            };
 
-            if (ImGui::Button(
-                    "Button"))  // Buttons return true when clicked (most
-                                // widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                        1000.0f / ImGui::GetIO().Framerate,
-                        ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window) {
-            ImGui::Begin(
-                "Another Window",
-                &show_another_window);  // Pass a pointer to our bool variable
-                                        // (the window will have a closing
-                                        // button that will clear the bool when
-                                        // clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me")) show_another_window = false;
             ImGui::End();
         }
 
