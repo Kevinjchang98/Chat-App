47215f9ebd4e4f30390a96c80e812ba2a65e914a
kevinchang <kevinjchang98@gmail.com>
2022-09-27 18:15:00 -0700

    remove unused #include

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,13 +1,13 @@
 #include <arpa/inet.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
+// #include <errno.h>
+// #include <netinet/in.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 #include <chrono>
 #include <cstring>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
+// #include <sys/socket.h>
+// #include <sys/types.h>
+// #include <unistd.h>
 
 #include <ctime>
 #include <iostream>
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,12 +1,12 @@
 #include <arpa/inet.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
+// #include <errno.h>
+// #include <netinet/in.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 #include <cstring>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
+// #include <sys/socket.h>
+// #include <sys/types.h>
+// #include <unistd.h>
 
 #include <ctime>
 #include <iostream>
