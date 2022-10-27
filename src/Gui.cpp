0140b41d87652280e0bc47a4c52f84f49d6d3783
Kevin Chang <kevinjchang98@gmail.com>
2022-10-26 23:41:57 -0700

    convert connectionDataIsValid() to static

--- src/Gui.cpp
+++ src/Gui.cpp
@@ -100,7 +100,7 @@ bool Gui::handleSend(char* text, std::shared_ptr<ChatHistory> history) {
  * @return true If both are valid
  * @return false If at least one is invalid
  */
-bool Gui::connectionDataIsValid() {
+static bool connectionDataIsValid() {
     // TODO: Check IP_ADDRESS and PORT are valid in format
     return true;
 }
